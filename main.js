0e36bfe5b259d9a1e2c788e283da6217d7b05ee4
boyonthebeach2k <39589879+boyonthebeach2k@users.noreply.github.com>
2021-10-26 12:58:07 +0530

    fixed env vars

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const client = new (require('discord.js')).Client({ intents: 32265, partials: ['CHANNEL', 'MESSAGE'] });//[Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.DIRECT_MESSAGES] });
 const { messageCreateHandler } = require('./commands/handlers');
 const TOKEN = (process.env.ENV === 'DEV') ? process.env.DEV_TOKEN : process.env.BOT_TOKEN;

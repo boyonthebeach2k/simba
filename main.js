dbb62193837620108dc1509e6d15043c1a44858d
boyonthebeach2k <39589879+boyonthebeach2k@users.noreply.github.com>
2021-10-28 10:55:23 +0530

    updated main

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const client = new (require('discord.js')).Client({ intents: 32265, partials: ['CHANNEL', 'MESSAGE'] });//[Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.DIRECT_MESSAGES] });
 const { messageCreateHandler } = require('./commands/handlers');
-const TOKEN = (process.argv[2].toLowerCase() === 'dev') ? process.env.DEV_TOKEN : process.env.BOT_TOKEN;
+const TOKEN = ((process.argv[2] || '').toLowerCase() === 'dev') ? process.env.DEV_TOKEN : process.env.BOT_TOKEN;
 
 client.on('ready', () => {
 
